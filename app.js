dc987e50e01a332f5fff6aa4db56c5d8e5c80c0d
Al Amin <alaminkhan10009@gmail.com>
2025-03-19 15:09:40 +0600

    add mongo-sanitize

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ const mongoose=require('mongoose')
 const cors=require('cors')
 const helmet=require('helmet');
 const rateLimit = require("express-rate-limit");
+const mongoSanitize=require('express-mongo-sanitize');
 const userRoutes=require('./routes/userRoutes');
 const postRoutes=require('./routes/postRoutes')
 const commentRoutes=require('./routes/commentRoutes');
 const eventRoutes=require('./routes/eventRoute')
+
 const app=express();
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(helmet());
+app.use(mongoSanitize());
 const limiter= rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 120, // Limit each IP to 100 requests per window
